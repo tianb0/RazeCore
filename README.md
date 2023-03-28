a1199900806e5c84f5d36cf218c29d91d2047875
Tianbo <tianbo@getjerry.com>
2023-03-28 13:36:02 -0700

    first api

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # RazeCore
 
-test dynamic lib
-
-```
-# create project: README, LICENSE, .gitignore
-
-# init Swift Package and name it via Xcode
-
-# specify .dynamic type in Package.swift
-
-# init xcode project
-swift package generate-xcodeproj
-
-```
+A description of this package.
--- Sources/RazeCore/RazeCore.swift
+++ Sources/RazeCore/RazeCore.swift
@@ -2,6 +2,13 @@ import UIKit
 
 public struct RazeCore {
     
+    /// Allows you to convert a 6 digit hexadecimal string into a UIColor instance
+    /// - Warning: The "#" symbol is stripped from the begining of the string submitted here.
+    /// - Parameters:
+    ///   - hexString: A 6-digit hexadecimal string. Use 6 digits rather than 8, and add the
+    ///   accompanying alpha value in the second parameter
+    ///   - alpha: A number between 0.0 and 1.0 indicating how transparent the color is
+    /// - Returns: A UIColor defined by the `hexString` parameter
     static func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
         let r, g, b: CGFloat
         let offset = hexString.hasPrefix("#") ? 1 : 0
